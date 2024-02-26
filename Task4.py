b7d8ef6c06a8969839fcae86a56c15f47e0b767c
Ostapenko Artem <a.ostapenko06@mail.ru>
2024-02-26 17:13:51 +0300

    Task4

---
+++ Task4.py
@@ -0,0 +1,22 @@
+import csv
+
+
+with open("game.txt", 'r', encoding='utf8') as fl:
+    data = list(csv.reader(fl, delimiter='$', quotechar='"'))
+    data = data[1:]
+
+counter = {}
+for line in data:
+    counter[line[0]] = counter.get(line[0], 0) + 1
+
+
+for line in data:
+    line.append(str(counter[line[0]]))
+
+ans = []
+for line in data:
+    ans.append('$'.join(line) + '\n')
+
+with open("game_counter.csv", 'w', encoding='utf8') as fl:
+    fl.write("GameName$characters$nameError$date$counter")
+    fl.writelines(ans)
